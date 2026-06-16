61bd4269afe000ae4a56c6157a8e39cf3deba955
OM MOHITE <omamohite@gmail.com>
2026-06-16 11:13:35 +0530

    Configure Dockerfile and standalone build for GCP Cloud Run

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import { withSentryConfig } from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: 'standalone',
 };
 
 export default withSentryConfig(nextConfig, {
